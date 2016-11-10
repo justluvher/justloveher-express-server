ec8fe2d81ccb8511fb537edba28221c01fa38c6a
yi kyu suk <justloveher79@gmail.com>
2016-11-11 01:32:02 +0900

    Update index.js
    render error

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(err,req, res, next) {
+  res.status(err.status || 500);
   res.render('index', { title: 'Express' });
 });
 
